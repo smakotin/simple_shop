5f7878701c4d4418ef73e5100e6fcfcb52c5fdee
Andrey Smakotin <slamfromm@gmail.com>
2022-03-02 21:14:12 +0300

    settings with .env

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -165,6 +165,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'test.django.smakotin@gmail.com' #TODO how to set env
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-
